525ed5d7b7fb09b595114e8ecbb70d821904d1cb
dot-360 <82105222+dot-360@users.noreply.github.com>
2022-04-06 20:28:32 -0400

    Add files via upload

---
+++ Lab1.js
@@ -0,0 +1,25 @@
+const questionOne = function questionOne(arr) {
+    // Implement question 1 here
+}
+
+const questionTwo = function questionTwo(arr) { 
+    // Implement question 2 here
+}
+
+const questionThree = function questionThree(arr) {
+    // Implement question 3 here
+}
+
+const questionFour = function questionFour(num1, num2, num3) {
+    // Implement question 4 here
+}
+
+module.exports = {
+    firstName: "YOUR FIRST NAME", 
+    lastName: "YOUR LAST NAME", 
+    studentId: "YOUR STUDENT ID",
+    questionOne,
+    questionTwo,
+    questionThree,
+    questionFour
+};
